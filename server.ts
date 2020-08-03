ad8928a051974824458cbf252b8cc1c5701eacb2
Kyle MacDonald <kyle@kylemacdonald.net>
2020-08-03 15:34:02 -0300

    Mongo is now connecting

--- server.ts
+++ server.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 8000;
 
+mongoose.connect('mongodb://localhost/rafflebox_assignment').then((res) => {
+  console.log('connected to db');
+}).catch((err) => {
+  console.log('error connecting to the db');
+  console.log(err);
+})
+
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
