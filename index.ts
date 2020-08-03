bfbbfe4d01f33b4f14dbd298249309e0139c2407
Kyle MacDonald <kyle@kylemacdonald.net>
2020-08-03 12:29:07 -0300

    Vue running

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-// rest of the code remains same
+
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
+
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
